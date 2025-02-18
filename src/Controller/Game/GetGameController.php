3fe38eaf1323133920932c80048dbe8b5c845f8e
baktybekovich1 <kydyrmysheva2005@gmail.com>
2025-02-18 14:36:05 +0500

    Documentation fix

--- src/Controller/Game/GetGameController.php
+++ src/Controller/Game/GetGameController.php
@@ -9,21 +9,29 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
 use OpenApi\Attributes as OA;
 
-#[OA\Tag(name:'Get api for Game')]
+#[OA\Tag(name: 'Get api for Game')]
 class GetGameController extends AbstractController
 {
     public function __construct(private readonly GetGameService $getGameService)
     {
     }
 
-
+    #[OA\Get(description: '
+    gameId: int,
+    winnerTeamId: int,
+    winnerTeamTitle: str,
+    winnerTeamScore: int,
+    loserTeamId: int,
+    loserTeamTitle: str,
+    loserTeamScore: int
+    ')]
     #[Route('/scores/{id}', name: 'app_game_scores', methods: ['GET'])]
     public function game_scores(Request $request): JsonResponse
     {
         return $this->json($this->getGameService->getGameScores($request->get('id')));
     }
 
-    #[Route('/goals/{id}', name: 'app_game_goals',methods: ['GET'])]
+    #[Route('/goals/{id}', name: 'app_game_goals', methods: ['GET'])]
     public function index(Request $request): JsonResponse
     {
         return $this->json($this->getGameService->getGameGoals($request->get('id')));
