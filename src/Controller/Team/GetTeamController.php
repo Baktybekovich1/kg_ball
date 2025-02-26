9c9903b052f37a769ad5282d9f1c289fbe414f7b
baktybekovich1 <kydyrmysheva2005@gmail.com>
2025-02-26 10:11:21 +0500

    fix bugs

--- src/Controller/Team/GetTeamController.php
+++ src/Controller/Team/GetTeamController.php
@@ -13,50 +13,50 @@ use OpenApi\Attributes as OA;
 #[OA\Tag(name:'Get api for Team')]
 class GetTeamController extends AbstractController
 {
-    public function __construct(
-        private readonly GetTeamService $getTeamService
-    )
-    {
-    }
-
-    /* Здесь Get запросы по командам (Team) перед путём каждого Route есть префикс /team;*/
-
-    #[Route('/progress/{id}', name: 'app_team_progress', methods: ['GET'])]
-    public function team_progress(Request $request): JsonResponse
-    {
-        return $this->json([$this->getTeamService->getProgress($request->get('id'))]);
-    }
-
-    #[Route('/game_info/{id}', name: 'app_team_game_info', methods: ['GET'])]
-    public function team_game_info(Request $request): JsonResponse
-    {
-        return $this->json([$this->getTeamService->getGameInfo($request->get('id'))]);
-    }
-
-    #[Route('/squad_list/{id}', name: 'app_team_squad_list', methods: ['GET'])]
-    public function team_squad_list(Request $request): JsonResponse
-    {
-        return $this->json(['players' => $this->getTeamService->getSquadList($request->get('id'))]);
-    }
-
-    #[Route('/game_list/{id}', name: 'app_team_game_list', methods: ['GET'])]
-    public function team_game_list(Request $request): JsonResponse
-    {
-        return $this->json(['games' => $this->getTeamService->getGameList($request->get('id'))]);
-    }
-
-
-    # Лучшие игроки команды 
-    #[Route('/best_players/{id}', name: 'app_team_best_players', methods: ['GET'])]
-    public function team_best_players(Request $request): JsonResponse
-    {
-        return $this->json(['players' => $this->getTeamService->getBestPlayers($request->get('id'))]);
-    }
-
-    #[Route('/game_statistics/{firstTeamId}/{secondTeamId}', name: 'app_team_game_statistics', methods: ['GET'])]
-    public function index(Request $request): JsonResponse
-    {
-        return $this->json($this->getTeamService->getGameStatistics($request->get('firstTeamId'), $request->get('secondTeamId')));
-    }
+//    public function __construct(
+//        private readonly GetTeamService $getTeamService
+//    )
+//    {
+//    }
+//
+//    /* Здесь Get запросы по командам (Team) перед путём каждого Route есть префикс /team;*/
+//
+//    #[Route('/progress/{id}', name: 'app_team_progress', methods: ['GET'])]
+//    public function team_progress(Request $request): JsonResponse
+//    {
+//        return $this->json([$this->getTeamService->getProgress($request->get('id'))]);
+//    }
+//
+//    #[Route('/game_info/{id}', name: 'app_team_game_info', methods: ['GET'])]
+//    public function team_game_info(Request $request): JsonResponse
+//    {
+//        return $this->json([$this->getTeamService->getGameInfo($request->get('id'))]);
+//    }
+//
+//    #[Route('/squad_list/{id}', name: 'app_team_squad_list', methods: ['GET'])]
+//    public function team_squad_list(Request $request): JsonResponse
+//    {
+//        return $this->json(['players' => $this->getTeamService->getSquadList($request->get('id'))]);
+//    }
+//
+//    #[Route('/game_list/{id}', name: 'app_team_game_list', methods: ['GET'])]
+//    public function team_game_list(Request $request): JsonResponse
+//    {
+//        return $this->json(['games' => $this->getTeamService->getGameList($request->get('id'))]);
+//    }
+//
+//
+//    # Лучшие игроки команды
+//    #[Route('/best_players/{id}', name: 'app_team_best_players', methods: ['GET'])]
+//    public function team_best_players(Request $request): JsonResponse
+//    {
+//        return $this->json(['players' => $this->getTeamService->getBestPlayers($request->get('id'))]);
+//    }
+//
+//    #[Route('/game_statistics/{firstTeamId}/{secondTeamId}', name: 'app_team_game_statistics', methods: ['GET'])]
+//    public function index(Request $request): JsonResponse
+//    {
+//        return $this->json($this->getTeamService->getGameStatistics($request->get('firstTeamId'), $request->get('secondTeamId')));
+//    }
 
 }
