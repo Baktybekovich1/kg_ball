fbd8d4110ab6e6698d5468c219d08a09ad27bf81
baktybekovich1 <kydyrmysheva2005@gmail.com>
2025-03-13 16:16:13 +0500

    Admin api's for Goal

--- src/Controller/MyAdmin/Goal/GoalController.php
+++ src/Controller/MyAdmin/Goal/GoalController.php
@@ -10,7 +10,9 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Attribute\MapRequestPayload;
 use Symfony\Component\Routing\Attribute\Route;
+use OpenApi\Attributes as OA;
 
+#[OA\Tag(name: 'Admin api for Goal')]
 class GoalController extends AbstractController
 {
     public function __construct(
