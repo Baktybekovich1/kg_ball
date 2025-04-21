ccd818be4c292353aa4a5b96c15c81cf7b97114c
baktybekovich1 <kydyrmysheva2005@gmail.com>
2025-04-21 16:00:25 +0500

    Auto commit settings

--- auto_commit.sh
+++ auto_commit.sh
@@ -1,30 +1,16 @@
 #!/bin/bash
 
-# Путь к вашему Symfony проекту
+# Перейти в директорию проекта
+cd /path/to/your/project
 
-$PROJECT_PATH = "."
+# Внести изменения (например, добавить временную метку в файл)
+echo "# Updated on $(date)" >> README.md
 
-# Файл, в который будем вносить изменения
-DUMMY_FILE="last_commit.log"
+# Добавить изменения в Git
+git add .
 
-# Переход в директорию проекта
-cd $PROJECT_PATH || exit
+# Закоммитить изменения
+git commit -m "Automated commit on $(date)"
 
-# Внесение фиктивных изменений
-echo "Последний коммит: $(date '+%Y-%m-%d %H:%M:%S')" > $DUMMY_FILE
-
-# Проверка на наличие изменений
-if [[ -n $(git status --porcelain) ]]; then
-    echo "Обнаружены изменения. Выполняю коммит и push."
-
-    # Добавить изменения
-    git add -A
-
-    # Создать коммит
-    git commit -m "Автоматический коммит: $(date '+%Y-%m-%d %H:%M:%S')"
-
-    # Отправить изменения в удалённый репозиторий
-    git push origin main
-else
-    echo "Нет изменений для коммита."
-fi
+# Отправить изменения на GitHub
+git push origin main
