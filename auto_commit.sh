b9a0f320e74215864bd2592b37bd7383610e03e5
baktybekovich1 <kydyrmysheva2005@gmail.com>
2025-08-14 17:47:01 +0600

    Automated commit on Thu Aug 14 05:47:01 PM +06 2025

--- auto_commit.sh
+++ auto_commit.sh
@@ -12,14 +12,4 @@ git commit -m "Automated commit on $(date)"
 # Отправить изменения на GitHub
 git push origin main
 
-echo "# Updated on $(date)" >> README.md
-
-# Добавить изменения в Git
-git add .
-
-# Закоммитить изменения
-git commit -m "Automated commit on $(date)"
-
-# Отправить изменения на GitHub
-git push origin main
 
